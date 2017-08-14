9d6bc2c9409927bcbafedf26a57f3fb74a0dad4b
Godfrey Huang <godfrey.huang@interpret.co.nz>
2017-08-14 17:43:51 +1200

    init()

---
+++ LeafletDemo/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace LeafletDemo.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ LeafletDemo/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LeafletDemo.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
